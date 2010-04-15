9877d83cf2283f22a4254753641ffa4198799420
perkinsms <mperkins@engineer.com>
2010-04-15 17:20:44 -0400

    modified:   GTFS.pm

--- GTFS.pm
+++ GTFS.pm
@@ -17,10 +17,15 @@ sub new {
     $self->{database} = $dbh;
     $self->{options} = $optref;
 	bless($self, $class);
+    $self->getTripsfromDB();
+    $self->getStopsfromDB();
+    $self->getRoutesfromDB();
+    $self->get_patterns();
     $self->initialize;
 }
 
 sub get_patterns {
+    my $self = shift;
     my $dbh = $self->{database};
     my $routes = $self->{routes};
     my $trips = $self->{trips};
@@ -29,14 +34,9 @@ sub get_patterns {
     # analyze the trips and routes to find the patterns
     my $patternquery = "select stop_sequence, stop_id from stop_times where trip_id = ? order by stop_sequence";
     my $sth = $dbh->prepare($patternquery);
-
-    my $tripinsertquery = "UPDATE trips SET pattern_id = ? WHERE trip_id = ?";
-    my $trip_st = $dbh->prepare($tripinsertquery);
-
     my $pattern_id = 0;
 
     foreach my $route (sort {$a->route_id <=> $b->route_id} values %$routes) {
-#foreach my $route (@$routes{qw(40 260 159 106 167 312)}) {
         my $patterns = {};
         foreach my $trip_id ($route->trips) {
             my $trip = $trips->{$trip_id};
@@ -83,102 +83,45 @@ sub get_patterns {
 
             #now let's update the sql database linking this trip
             #to a particular pattern
-
-            $trip_st->execute($pattern_id, $trip_id) 
-                or die "Could not execute trip insertion query";
+            $trips->{$trip_id}{pattern} = $pattern_id; 
         }
 
-        $route->push_patterns( values %patterns );
-        print $route->route_short_name . " Completed!\n";
+        $route->push_patterns( values %$patterns );
 
-        print OUTPUT "\n";
-        print OUTPUT "Finished " . $route->route_id 
-            . " " . $route->route_short_name . "\n";
-
-        foreach my $i (sort {$a->pattern_id <=> $b->pattern_id} values %patterns) {
-            print OUTPUT "Pattern: " . $i->pattern_id 
-                . "\tTrip: " . $i->trip 
-                . "\tStops: " .  ($#{ $i->{STOPS} } + 1) 
-                . "\tFirst: " . $i->{STOPS}[0] 
-                . "\tLast: " . $i->{STOPS}[-1] 
-                . "\tCount: " . $i->{COUNT} 
-                . "\n";
-        }
     }
 
+    foreach my $route (sort {$a->route_id <=> $b->route_id} values %$routes) {
+        foreach my $pattern (sort {$a->pattern_id <=> $b->pattern_id} $route->patterns) {
 
-#print out a csv file that can be sourced into sql
-#I'd do this for the trips, but it was so large it couldn't be inserted
-foreach my $route (sort {$a->route_id <=> $b->route_id} values %routes) {
-    foreach my $pattern (sort {$a->pattern_id <=> $b->pattern_id} $route->patterns) {
+            my $length = $#{$pattern->indexes};
+            my @dist = (0);
+            my @stopids = $pattern->stops;
+            for (my $i = 1; $i <= $length; $i++) {
+                my $a = $stops->{$stopids[$i-1]};
+                my $b = $stops->{$stopids[$i]};
+                push @dist, ($dist[$i-1] + $a->twostopdist($b));
+            }
+            $pattern->distances(\@dist);
+            $pattern->totaldist($dist[-1]);
 
-        my $length = $#{$pattern->indexes};
-
-        my @dist = (0);
-
-        my @stopids = $pattern->stops;
-
-
-        for (my $i = 1; $i <= $length; $i++) {
-            my $a = $stops{$stopids[$i-1]};
-            my $b = $stops{$stopids[$i]};
-            push @dist, ($dist[$i-1] + $a->twostopdist($b));
-        }
-
-        $pattern->distances(\@dist);
-        $pattern->totaldist($dist[-1]);
-
-        for (my $i = 0; $i <= $length; $i++) {
-            print PATOUT join(',', 
-                $route->route_id, 
-                $pattern->pattern_id, 
-                $pattern->{INDEXES}[$i], 
-                $pattern->{STOPS}[$i], 
-                $pattern->{DISTANCES}[$i])
-                . "\n";
        }
 
     }
 }
 
-sub initialize {
+sub getStopsfromDB {
     my $self = shift;
-    my $dbh = $self->{database};
+    $self->{stops} = Stop->fromDB($self->{database});
+}
 
-    my @required_tables = qw(agency stops routes trips stop_times calendar);
-    my @optional_tables = qw(calendar_dates fare_rules fare_attributes shapes frequencies transfers);
-    my @built_tables = qw(patterns);
+sub getTripsfromDB {
+    my $self = shift;
+    $self->{trips} = Trip->fromDB($self->{database});
+}
 
-    my @tables = $dbh->tables();
+sub getRoutesfromDB {
+    my $self = shift;
+    $self->{routes} = Route->fromDB($self->{database});
+}
 
-    foreach my $table (@required_tables) {
-        if (grep {$table eq $_} @tables) {
-            print "$table is present\n";
-        } else {
-            die "Required table $table is missing\n";
-        }
-    }
-
-    foreach my $table (@optional_tables) {
-        if (grep {$table eq $_} @tables) {
-            print "optional table $table is present\n";
-        } else {
-            print "optional table $table not present\n";
-        }
-    }
-
-    foreach my $table (@built_tables) {
-        if (grep {$table eq $_ } @tables) {
-            print "built table $table is present\n";
-        } else {
-            print "built table $table not present\n";
-        }
-    }
-
-    # Ensure stops table has adequate columns
-    # required columns: 
-    # stop_id
-    # stop_name
-    # stop_lat
-    # stop_lon
-
+1;
