ca6dd6802b0de09d92d3efec0f7f9af1c3b09f72
perkinsms <mperkins@engineer.com>
2010-04-29 07:25:18 -0400

    Better script to load sql

--- loadfromfile.pl
+++ loadfromfile.pl
@@ -13,6 +13,8 @@ my $data = {
 
 my @tablenames = qw/agency calendar calendar_dates routes stops trips stop_times/;
 
+open($data->{fh}, '>', $data->{path_to_data} . "/" . $data->{database} . "/load-data.sql") or die "Could not open file for writing SQL commands: $!";
+
 foreach my $table (@tablenames) {
     print "Loading $table\n";
     loadtable($table, $data);
@@ -26,13 +28,14 @@ sub loadtable {
     my $database = $data->{database};
     my $username = $data->{username};
     my $password = $data->{password};
+    my $outfh = $data->{fh};
 
     my $csvparser = Text::CSV_XS->new( { binary => 1, blank_is_undef => 1, empty_is_undef => 1, allow_whitespace => 1} );
     my $csv_fh = Tie::Handle::CSV->new("$path_to_data/$database/$table.txt", header => 1, csv_parser => $csvparser );
     my @fieldslist = @{$csv_fh->header};
     my $fieldstring = "(" . (join ",", @fieldslist) . ")";
     
-    my $loaddataquery = "LOAD DATA LOCAL INFILE '$path_to_data/$database/$table.txt' REPLACE INTO TABLE $table COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\r\n' IGNORE 1 LINES ";
+    my $loaddataquery = "LOAD DATA LOCAL INFILE '$path_to_data/$database/$table.txt' REPLACE INTO TABLE $table COLUMNS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY \'\\r\\n\' IGNORE 1 LINES ";
 
     if ($table eq "stop_times") {
         $fieldstring =~ s/arrival_time/\@avar/;
@@ -44,9 +47,19 @@ sub loadtable {
     }
     close $csv_fh;
 
-    my $dbh = DBI->connect("DBI:mysql:database=$database",$username,$password) or die "Could not connect: $DBI::errstr";
 
-    $dbh->do($loaddataquery) or die "Could not load data from $table.txt: " . $dbh->errstr;
+    #my $dbh = DBI->connect("DBI:mysql:database=$database",$username,$password) or die "Could not connect: $DBI::errstr";
 
-    $dbh->disconnect;
+    #$dbh->do("TRUNCATE TABLE $table") or die "Could not delete all data from $table" . $dbh->errstr;
+
+
+    print $outfh "TRUNCATE TABLE $table\;\n";
+
+    #$dbh->do($loaddataquery) or die "Could not load data from $table.txt: " . $dbh->errstr;
+
+    print $outfh "$loaddataquery\;\n";
+
+    print $outfh "\n";
+
+    #$dbh->disconnect;
 }
--- mysqltestinout.pl
+++ mysqltestinout.pl
@@ -3,14 +3,16 @@ use strict;
 use GTFS;
 use DBI;
 
-my $gtfs_dir = "input/arlington_gtfs";
+my $databasename = "sfmta_gtfs";
+
+my $gtfs_dir = "input/$databasename";
 my $dbh = DBI->connect("DBI:CSV:f_dir=$gtfs_dir;f_ext=.txt;csv_sep_char=,;csv_quote_char=\"") or die "Could not connect: $DBI::errstr";
 
-my $gtfs = GTFS->new($dbh,{patterns => 0});
+my $gtfs = GTFS->new($dbh,{patterns => 0, loaddata => 0});
 
 my $username = "mperkins";
 my $password = "secret";
-my $database = "arlington_gtfs";
+my $database = "$databasename";
 my $dbhout = DBI->connect("DBI:mysql:database=$database",$username,$password) or die "Could not connect: $DBI::errstr";
 
 #$gtfs->writeAlltoDB($dbhout);
